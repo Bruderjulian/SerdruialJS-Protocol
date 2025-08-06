a6781621dedb379c0edbd898db573f859a71ffad
BruderJulian <100963187+Bruderjulian@users.noreply.github.com>
2025-08-06 17:25:25 +0000

    created plugin registry 1/2

---
+++ registry/examplePlugin.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "test",
+  version: "1.0.0",
+  description: "A Test Plugin",
+  author: "BruderJulian",
+  license: "MIT",
+  dependencies: [],
+  priority: 1,
+
+  load: () => {
+
+  },
+  unload: () => {
+
+  },
+  install: () => {
+
+  },
+  uninstall: () => {
+
+  },
+};
