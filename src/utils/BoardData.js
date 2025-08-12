2f7b30f4cd1b1a21a16788e3b0e20f061c74447f
BruderJulian <100963187+Bruderjulian@users.noreply.github.com>
2025-08-12 17:39:46 +0200

    Save BoardData.js.

--- src/utils/BoardData.js
+++ src/utils/BoardData.js
@@ -1,3 +1,4 @@
+import {isObject, IsArray} from "./utils.js";
 
 export default class BoardData {
   id;
@@ -11,6 +12,34 @@ export default class BoardData {
     this.pins = parsePins(pins);
     this.spec = spec;
   }
+  
+  static create(id, name, version = "1", pins = "", spec = {}) {
+      if (isObject(id)) {
+          id = obj.id;
+          name = obj.name ?? name;
+          version = obj.version ?? version;
+          pins = obj.pins ?? pins;
+          spec = obj.spec ?? spec;
+      }
+      if (typeof id !== "string" || typeof name !== "string") {
+        throw new TypeError("Invalid Name or Id");
+      }
+      if (typeof version !== "string" ) {
+        throw new TypeError("Invalid Version");
+      }
+      if (typeof pins !== "string") {
+        throw new TypeError("Invalid Pins"); 
+      }
+      if (!isObject(spec) {
+        throw new TypeError("Invalid Spec");
+      }
+      return new BoardData(id, name, version, pins, spec);
+  }
+  
+  static find() {
+      
+  }
+  
 
   static parsePins() {
     
